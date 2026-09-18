66c2304a32a8ca2f29eab96c3df8882ab4d681e8
Yato <31593714+YatoGitHub@users.noreply.github.com>
2026-09-18 13:14:59 +0300

    Update MyRoom.js

--- MyRoom.js
+++ MyRoom.js
@@ -2,8 +2,31 @@ const CURRENT_CHAR_ID = document
     .querySelector('#room_form')
     .getAttribute('data-charid');
 
-document.querySelectorAll('#talks .t').forEach(message => {
+function processMessage(message) {
     if (message.getAttribute('data-charid') === CURRENT_CHAR_ID) {
         message.classList.add('my-message');
     }
+}
+
+// Обрабатываем сообщения, которые уже есть
+document.querySelectorAll('#talks .t').forEach(processMessage);
+
+// Следим за новыми сообщениями
+const observer = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+
+            if (node.nodeType !== 1) {
+                continue;
+            }
+
+            if (node.classList.contains('t')) {
+                processMessage(node);
+            }
+        }
+    }
+});
+
+observer.observe(document.getElementById('talks'), {
+    childList: true
 });
