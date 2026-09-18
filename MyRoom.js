cb40a36da7af089f4e1636db6991bf3d73de2d5f
Yato <31593714+YatoGitHub@users.noreply.github.com>
2026-09-18 13:06:41 +0300

    Update MyRoom.js

--- MyRoom.js
+++ MyRoom.js
@@ -1,1 +1,9 @@
-alert('JS ЗАПУСТИЛСЯ');
+const CURRENT_CHAR_ID = document
+    .querySelector('#room_form')
+    .getAttribute('data-charid');
+
+document.querySelectorAll('#talks .t').forEach(message => {
+    if (message.getAttribute('data-charid') === CURRENT_CHAR_ID) {
+        message.classList.add('my-message');
+    }
+});
